5cd0b1eee71510f93112b19436940daf37834b64
Derek <derekanderson8@msn.com>
2021-03-09 15:03:26 -0800

    updated loop.js

---
+++ AlgorithmPractice/loops.js
@@ -0,0 +1,14 @@
+//1. Print odds 1-20
+for(n=1;n<20;n++){
+    if(n % 2 == 1){
+        console.log(n)
+    }
+}
+
+//2. Sum and Print 1-5
+var sum = 0;
+for(num = 1;num <= 5; num ++){
+    sum = sum + num;
+    console.log('Num: '+ num + ', Sum: '+ sum)
+}
+
