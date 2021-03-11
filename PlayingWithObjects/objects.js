f5c3a1f935a088cf48b144c96cb65cba0a77b53a
Derek <derekanderson8@msn.com>
2021-03-11 08:33:30 -0800

    Completed Objects

--- PlayingWithObjects/objects.js
+++ PlayingWithObjects/objects.js
@@ -18,5 +18,5 @@ console.log(users[1].age);
 console.log(users[0].name);
 //How would you print/log the name and age of each user using a for loop?
 for (i = 0; i < users.length; i++) {
-    console.log(users[i].name + " - " + users[i].age);
+    console.log(users[i].name, "-", users[i].age);
 }
